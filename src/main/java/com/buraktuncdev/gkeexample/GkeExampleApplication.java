e223057bfefd279483361dd1a59dfe58e1fe29f5
Burak Tunc <burak.tunc@check24.de>
2021-01-12 10:35:33 +0100

    first commit

---
+++ src/main/java/com/buraktuncdev/gkeexample/GkeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.buraktuncdev.gkeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buraktuncdev/gkeexample/GkeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buraktuncdev.gkeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
